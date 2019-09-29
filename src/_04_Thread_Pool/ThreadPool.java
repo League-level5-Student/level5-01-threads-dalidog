74f37cc0a182ab2a35704e64ac39860063eb02f1
dalidog <daliasebat@gmail.com>
2019-09-29 11:32:23 -0700

    started threadpool

--- src/_04_Thread_Pool/ThreadPool.java
+++ src/_04_Thread_Pool/ThreadPool.java
@@ -1,5 +1,29 @@
 package _04_Thread_Pool;
 
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class ThreadPool {
+ArrayList threadarray = new ArrayList();
+ConcurrentLinkedQueue<Task> taskq = new ConcurrentLinkedQueue<Task>();
+	public ThreadPool(int i) {
+		// TODO Auto-generated constructor stub
+		for (int j = 0; j < i; j++) {
+			Thread x = new Thread(new Worker());
+			threadarray.add(x);
+		}
+		
+	}
+
+	public void addTask(Task task) {
+		// TODO Auto-generated method stub
+		taskq.add(task);
+		
+	}
+
+	public void start() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
