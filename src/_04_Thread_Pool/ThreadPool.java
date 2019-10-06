862b95eb970d987500846535cb88205bd1414d11
League Student <league@iMac-8.attlocal.net>
2019-10-06 11:26:52 -0700

    finished thread pool

--- src/_04_Thread_Pool/ThreadPool.java
+++ src/_04_Thread_Pool/ThreadPool.java
@@ -4,26 +4,32 @@ import java.util.ArrayList;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class ThreadPool {
-ArrayList threadarray = new ArrayList();
+ArrayList<Thread> threadarray = new ArrayList<>();
 ConcurrentLinkedQueue<Task> taskq = new ConcurrentLinkedQueue<Task>();
 	public ThreadPool(int i) {
 		// TODO Auto-generated constructor stub
 		for (int j = 0; j < i; j++) {
-			Thread x = new Thread(new Worker());
+			Thread x = new Thread(new Worker(taskq));
+	
 			threadarray.add(x);
 		}
 		
 	}
 
-	public void addTask(Task task) {
-		// TODO Auto-generated method stub
-		taskq.add(task);
-		
-	}
 
 	public void start() {
 		// TODO Auto-generated method stub
+		threadarray.stream().forEach(thread->thread.start());
 		
 	}
+
+
+
+	public void addTask(Task task) {
+		taskq.add(task);
+	}
+
+
+	
 
 }
--- src/_04_Thread_Pool/Worker.java
+++ src/_04_Thread_Pool/Worker.java
@@ -1,12 +1,19 @@
 package _04_Thread_Pool;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class Worker implements Runnable {
-
+	ConcurrentLinkedQueue<Task> taskq;
+public Worker(ConcurrentLinkedQueue<Task>  taskq) {
+	this.taskq=taskq;
+}
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
+		while(taskq.size()!=0) {
+			taskq.remove().perform();
 		
+		}
 	}
 
 
